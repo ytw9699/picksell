91d61878ae6b8980830859385a11e688d2d0d6ee
ytw9699 <ytw9699@naver.com>
2018-06-27 16:58:27 +0900

    1

--- picksell/src/main/java/com/mycom/config/CommandMap.java
+++ picksell/src/main/java/com/mycom/config/CommandMap.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-public class CommandMap {
+public class CommandMap {//1
 	
 	Map<String, Object> map = new HashMap<String, Object>();
 	
