a50381daa8e48ad43a7c8ff389798afda62e334f
dhrkawk111 <40296254+dhrkawk111@users.noreply.github.com>
2018-06-27 15:32:55 +0900

    마스터 정리커밋
    작성자:박현우

--- picksell/src/main/java/com/mycom/picksell/A.java
+++
@@ -1,6 +0,0 @@
-package com.mycom.picksell;
-
-public class A {
-	// 원격 마스터에 올라가있는 좋은 코드의 A 클래스의 코드
-	// 
-}
--- picksell/src/main/java/com/mycom/validator/Second.java
+++
@@ -1,9 +0,0 @@
-package com.mycom.validator;
-
-public class Second {
-//second
-	//commit1
-	//수정되었어요 원래는 commit2 가 있던자리
-	//commit3
-	//한글이? 브랜치를 위한
-}
