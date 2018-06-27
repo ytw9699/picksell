2d17ccf8ca1f7f9ae2fa55deb6847da58009a1e3
dhrkawk111 <40296254+dhrkawk111@users.noreply.github.com>
2018-06-27 20:22:13 +0900

    D

--- picksell/src/main/java/com/mycom/client_product/ProductController.java
+++ picksell/src/main/java/com/mycom/client_product/ProductController.java
@@ -4,5 +4,24 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class ProductController {
+	
+	/*픽셀플러스 인기상품 전체보기
+	새로등록된 플러스상품 전체보기
+	새로등록된 일반상품 전체보기
 
+	상품등록폼(메인에서)
+
+	ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+	픽셀플러스(헤더에서) -> 카테고리 분류있음
+	일반상품(헤더에서) -> 카테고리 분류있음
+	새로등록된상품(헤더에서) -> 카테고리 분류없음
+	
+	카테고리별 
+	DD
+	*/
+	
+	
+	
+
+	
 }
