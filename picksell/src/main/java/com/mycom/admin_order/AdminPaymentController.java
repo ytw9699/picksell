a13d3371b06eed6b730350048db2471422b4faac
Hyemin <khm110611@naver.com>
2018-06-27 21:17:41 +0900

    check

--- picksell/src/main/java/com/mycom/admin_order/AdminPaymentController.java
+++ picksell/src/main/java/com/mycom/admin_order/AdminPaymentController.java
@@ -1,6 +1,23 @@
 package com.mycom.admin_order;
 
-public class AdminPaymentController {
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdminPaymentController {
+	
+	AdminPaymentController(){
+		
+	}
+	
+	
+	
 }
-//안녕하세요 
