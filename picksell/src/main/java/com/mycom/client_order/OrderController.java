89ac0263abcb090850fb8377021a49ca11344560
dhrkawk111 <40296254+dhrkawk111@users.noreply.github.com>
2018-07-05 18:45:48 +0900

    임시커밋

--- picksell/src/main/java/com/mycom/client_order/OrderController.java
+++ picksell/src/main/java/com/mycom/client_order/OrderController.java
@@ -1,5 +1,7 @@
 package com.mycom.client_order;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,11 +9,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class OrderController {
 	
+	@Resource(name="orderService")
+	private OrderService orderService;
+	
 	@RequestMapping(value="/purchase/order", method=RequestMethod.POST)
 	public String orderProc(OrderModel orderModel) {
 		
 		
-		System.out.println(orderModel.getO_list().size());
+		System.out.println("ordercontroller:::"+orderModel.getO_list().size());
+		
+		
 		
 		
 		return "orderSuccess";
